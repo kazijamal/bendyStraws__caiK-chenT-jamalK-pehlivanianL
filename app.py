f472f10fbd5b1e2e556fb15be25479e0442e07b4
Kazi Jamal <jamalkazi02@gmail.com>
2019-10-22 14:54:19 -0400

    made createstory page and updated app.py with create story routes

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app = Flask(__name__)
 def root():
     return __name__;
 
+# page for creating a new story
+@app.route("/createstory")
+def createStory():
+    return render_template("createstory.html")
+
+# route for creating a new story
+@app.route("/newstory", methods=['POST'])
+def newStory():
+    return "new story route"
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
