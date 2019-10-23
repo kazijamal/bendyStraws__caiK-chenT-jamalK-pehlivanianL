7d83a7825de48576d61b8a69d6bc488483f62d79
Kazi Jamal <jamalkazi02@gmail.com>
2019-10-23 11:55:42 -0400

    updated database.py file

--- databases.py
+++ databases.py
@@ -1,7 +1,6 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
-
 DB_FILE="odyssey.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
@@ -10,24 +9,29 @@ c = db.cursor()               #facilitate db ops
 #==========================================================
 
 c.execute("DROP TABLE IF EXISTS stories");
-c.execute("CREATE TABLE IF NOT EXISTS stories (id INTEGER, name TEXT)");
+c.execute("CREATE TABLE IF NOT EXISTS stories (storyID INTEGER PRIMARY KEY, name TEXT)");
 
-c.execute("DROP TABLE IF EXISTS story-edits");
-c.execute("CREATE TABLE IF NOT EXISTS story-edits (storyID INTEGER, userID INTEGER, content TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)");
+c.execute("DROP TABLE IF EXISTS story_edits");
+c.execute("CREATE TABLE IF NOT EXISTS story_edits (storyID INTEGER, userID INTEGER, content TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)");
 
 c.execute("DROP TABLE IF EXISTS users");
-c.execute("CREATE TABLE IF NOT EXISTS users (userID INTEGER, username TEXT, password, TEXT)");
+c.execute("CREATE TABLE IF NOT EXISTS users (userID INTEGER, username TEXT, password TEXT)");
 
-#=========================================================
+#==========================================================
+storyID = 1
 
-storyID = 0
-
-def createStory(title, text, userID):
+def createStory(title, content, userID):
     #create new record in stories table
-    c.execute("INSERT INTO stories values(%d,'%s')", % (storyID, title))
+    c.execute("INSERT INTO stories VALUES (NULL, ?)", (title, ))
 
     #create new record in story_edits TABLE
-    c.execute("INSERT INTO story_edits values(%d, %d, '%s')", %(storyID, userID, text))
+    c.execute("INSERT INTO story_edits VALUES (?, ?, ?, NULL)", (storyID, userID, content))
 
-    #increment storyID
-    storyID = storyID + 1
+    storyId = storyID + 1
+
+createStory("title", "this is the content of the story", 5)
+
+#==========================================================
+
+db.commit()
+db.close()
