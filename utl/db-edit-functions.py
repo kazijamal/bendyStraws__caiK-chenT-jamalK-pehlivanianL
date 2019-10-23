36a41026561d4e4be5d12871a9e77cf410cbc6ce
Kevin Cai <k1029384756c@gmail.com>
2019-10-23 14:45:46 -0400

    Update db-edit-functions.py

--- utl/db-edit-functions.py
+++ utl/db-edit-functions.py
@@ -1,3 +1,4 @@
+from utl import dbfunctions
 #EDIT STORIES
 def addToStory(c, storyID, content, userID):
     c.execute("INSERT INTO story_edits VALUES (?, ?, ?, NULL)", (storyID, userID, content))
@@ -11,3 +12,11 @@ def getContributedStories(c, userID):
 def getNotContributedStories(c, userID):
     #c.execute("SELECT stories.storyID, userID \n FROM stories \nLEFT JOIN story_edits WHERE userID = "+str(userID)+" USING(storyID) \nWHERE userID IS NULL")
     return c.fetchall()
+
+# DEBUG:
+def debugAdd():
+    dbfunctions.createStory("HelloWorld","hi",0)
+    addToStory(2," world", 5)
+    debugPrintSelect("story_edits")
+    print(str(getContributedStories(5)))
+    print(str(getNotContributedStories(5)))
