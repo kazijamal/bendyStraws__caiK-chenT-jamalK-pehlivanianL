acf4873f1076faba642963c5a7fcbe69a81fd28c
kevin16777216 <k1029384756c@gmail.com>
2019-10-26 20:36:37 -0400

    full format

--- utl/dbfunctions.py
+++ utl/dbfunctions.py
@@ -1,58 +1,57 @@
 def createTables(c):
-    c.execute("CREATE TABLE IF NOT EXISTS stories (storyID INTEGER PRIMARY KEY, name TEXT)");
+    c.execute('CREATE TABLE IF NOT EXISTS stories (storyID INTEGER PRIMARY KEY, name TEXT)'
+              )
 
-    c.execute("CREATE TABLE IF NOT EXISTS story_edits (storyID INTEGER, userID INTEGER, username STRING, content TEXT, timestamp DATETIME)");
+    c.execute('CREATE TABLE IF NOT EXISTS story_edits (storyID INTEGER, userID INTEGER, username STRING, content TEXT, timestamp DATETIME)'
+              )
 
-    c.execute("CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY, username TEXT, password TEXT)");
+    c.execute('CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY, username TEXT, password TEXT)'
+              )
+
 
 def dropTables(c):
-    c.execute("DROP TABLE IF EXISTS stories")
-    c.execute("DROP TABLE IF EXISTS story_edits")
-    c.execute("DROP TABLE IF EXISTS users")
+    c.execute('DROP TABLE IF EXISTS stories')
+    c.execute('DROP TABLE IF EXISTS story_edits')
+    c.execute('DROP TABLE IF EXISTS users')
+
 
 def getSearch(c, query):
     c.execute("SELECT * FROM stories WHERE name LIKE '%" + query + "%'")
     return c.fetchall()
 
-#returns an array with all values from a given story's row in table.
-#find this story by its id
+
+# returns an array with all values from a given story's row in table.
+# find this story by its id
+
 def selectStory(c, storyID):
-    c.execute("SELECT name FROM stories WHERE storyID = ?", (storyID, ))
+    c.execute('SELECT name FROM stories WHERE storyID = ?', (storyID, ))
     return c.fetchone()
 
-#returns a tuple with all story names
+
+# returns a tuple with all story names
+
 def returnStoryNames(c):
-    c.execute("SELECT name FROM stories")
+    c.execute('SELECT name FROM stories')
     return c.fetchall()
 
-#returns an array with entire table as data.
+
+# returns an array with entire table as data.
+
 def getTable(c, table):
-    c.execute("SELECT * FROM " + table)
+    c.execute('SELECT * FROM ' + table)
     return c.fetchall()
+
 
 def getMaxStoryID(c):
-    c.execute("SELECT MAX(storyID) FROM stories")
+    c.execute('SELECT MAX(storyID) FROM stories')
     return c.fetchone()[0]
 
-def getUsername(c,userID):
-    c.execute("SELECT username FROM users WHERE userID = " + userID)
+
+def getUsername(c, userID):
+    c.execute('SELECT username FROM users WHERE userID = ' + userID)
     return c.fetchone()[1]
-    
+
+
 def debugPrintSelect(c, table):
-    c.execute("SELECT * FROM " + table)
-    print(str(c.fetchall()) + "\n")
-
-
-# TESTING
-
-# import sqlite3
-#
-# DB_FILE = "odyssey.db"
-#
-# db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-# c = db.cursor() #facilitate db operations
-# createTables(c)
-#
-# debugPrintSelect(c, "stories")
-# debugPrintSelect(c, "story_edits")
-# debugPrintSelect(c, "users")
+    c.execute('SELECT * FROM ' + table)
+    print (str(c.fetchall()) + '\n')
