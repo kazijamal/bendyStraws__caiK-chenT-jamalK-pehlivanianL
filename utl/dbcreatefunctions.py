d85763038cb5618c2274a8731487014b1ccb22fb
Tammy Chen <tchen00@stuy.edu>
2019-10-27 17:42:36 -0400

    fix formatting + added more comments to dbeditfxns

--- utl/dbcreatefunctions.py
+++ utl/dbcreatefunctions.py
@@ -1,13 +1,6 @@
 from utl import dbeditfunctions
 
-
-def createStory(
-    c,
-    storyID,
-    title,
-    userID,
-    username,
-    content,
-    ):
+# function that takes the given parameters to create a story & add it to the database
+def createStory(c, storyID, title, userID, username, content,):
     c.execute('INSERT INTO stories VALUES (NULL, ?)', (title, ))
     dbeditfunctions.addToStory(c, storyID, userID, username, content)
